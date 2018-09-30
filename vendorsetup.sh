6258dc00bc0bb92be1b8647285d3b1fee701116c
Manish Vora <manish@vora.me>
2018-09-30 14:02:27 +0000

    We build hlte

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,3 @@
-for combo in $(curl -s https://raw.githubusercontent.com/LineageOS/hudson/master/lineage-build-targets | sed -e 's/#.*$//' | grep cm-14.1 | awk '{printf "lineage_%s-%s\n", $1, $2}')
-do
-    add_lunch_combo $combo
+for var in eng user userdebug; do
+add_lunch_combo lineage_hlte-$var
 done
